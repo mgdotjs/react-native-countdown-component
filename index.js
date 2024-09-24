33e106db4593fd2b8959ecaec5e248af4bcc31d4
Mehmet Gündüz <mehmetgunduzzz@gmail.com>
2024-09-25 00:50:52 +0300

    'removeEventListener' problem solved

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ class CountDown extends React.Component {
     until: Math.max(this.props.until, 0),
     lastUntil: null,
     wentBackgroundAt: null,
+    eventListener: null, // NEW LINE
   };
 
   constructor(props) {
@@ -51,12 +52,14 @@ class CountDown extends React.Component {
   }
 
   componentDidMount() {
-    AppState.addEventListener('change', this._handleAppStateChange);
+    // AppState.addEventListener('change', this._handleAppStateChange);
+    this.state.eventListener = AppState.addEventListener('change', this._handleAppStateChange); // MODIFIED
   }
 
   componentWillUnmount() {
     clearInterval(this.timer);
-    AppState.removeEventListener('change', this._handleAppStateChange);
+    // AppState.removeEventListener('change', this._handleAppStateChange);
+    this.state.eventListener.remove(); // MODIFIED
   }
 
   componentDidUpdate(prevProps, prevState) {
